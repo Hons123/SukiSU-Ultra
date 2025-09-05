ddefc4d07d5431e7bfc417116a5471b9f5c4e508
Hons <87039304+Hons123@users.noreply.github.com>
2025-09-06 03:33:12 +0800

    Update apk_sign.c

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -30,6 +30,7 @@ static struct apk_sign_key {
 	const char *sha256;
 } apk_sign_keys[] = {
 	{EXPECTED_SIZE_SHIRKNEKO, EXPECTED_HASH_SHIRKNEKO}, // ShirkNeko/SukiSU
+    {EXPECTED_SIZE_JINITAIMEI, EXPECTED_HASH_JINITAIMEI}, // 鸡你太美
 	{EXPECTED_SIZE_OTHER, EXPECTED_HASH_OTHER}, // Dynamic Sign
 #ifdef EXPECTED_SIZE
 	{EXPECTED_SIZE, EXPECTED_HASH}, // Custom
