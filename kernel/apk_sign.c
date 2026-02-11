ae37ae5fa6b76498ebc9d91d4dd50371a0fe957f
Hons <87039304+Hons123@users.noreply.github.com>
2026-02-11 19:23:08 +0800

    Update apk_sign.c

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -25,8 +25,8 @@ struct sdesc {
 };
 
 static apk_sign_key_t apk_sign_keys[] = {
-    { EXPECTED_SIZE_SHIRKNEKO, EXPECTED_HASH_SHIRKNEKO }, // ShirkNeko/SukiSU
 	{ EXPECTED_SIZE_HONS, EXPECTED_HASH_HONS },           // ikunSU
+    { EXPECTED_SIZE_SHIRKNEKO, EXPECTED_HASH_SHIRKNEKO }, // ShirkNeko/SukiSU
 #ifdef EXPECTED_SIZE
     { EXPECTED_SIZE, EXPECTED_HASH }, // Custom
 #endif
