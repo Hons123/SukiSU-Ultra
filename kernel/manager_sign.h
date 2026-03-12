b4481864f6d976a645b26be5ad940dde3cabc05a
Hons <87039304+Hons123@users.noreply.github.com>
2026-03-12 20:07:42 +0800

    Update manager_sign.h

--- kernel/manager_sign.h
+++ kernel/manager_sign.h
@@ -1,6 +1,11 @@
 #ifndef MANAGER_SIGN_H
 #define MANAGER_SIGN_H
 
+// Hons/KernelSU
+#define EXPECTED_SIZE_HONS 0x316
+#define EXPECTED_HASH_HONS                                                    \
+	"983f5d3fd2f27cabd963b59deefdadfafa051017a6f0fb1573cb0f2d14b80842"
+
 // ShirkNeko/SukiSU
 #define EXPECTED_SIZE_SHIRKNEKO 0x35c
 #define EXPECTED_HASH_SHIRKNEKO                                                \
